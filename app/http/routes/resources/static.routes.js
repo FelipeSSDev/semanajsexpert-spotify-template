71306fccafa487167826b2094774e947e9ec029f
FelipeSSDev <felipedevbrasil@gmail.com>
2022-03-19 08:41:07 -0300

    feat: implement content type on static files route

--- app/http/routes/resources/static.routes.js
+++ app/http/routes/resources/static.routes.js
@@ -1,14 +1,20 @@
+import config from "../../../config/index.js";
 import { StreamController } from "../../controller/index.js";
 import { StreamService } from "../../service/index.js";
+
+const {
+  constants: { CONTENT_TYPE },
+} = config;
 
 const streamService = new StreamService();
 const streamController = new StreamController(streamService);
 
 const index = async (request, response) => {
-  const { url } = request;
+  const { url, type } = request;
 
   const { stream } = await streamController.getFileStream(url);
 
+  response.writeHead(200, CONTENT_TYPE[type]);
   stream.pipe(response);
 };
 
