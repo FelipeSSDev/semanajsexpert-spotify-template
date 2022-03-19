2981a49e95bc88ae7e9a174e33cdebd1d6be9d10
FelipeSSDev <felipedevbrasil@gmail.com>
2022-03-19 05:40:25 -0300

    feat: create a configuration file

---
+++ app/config/index.js
@@ -0,0 +1,26 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import.meta.url;
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const root = join(currentDir, "../../");
+const audioDir = join(root, "audio");
+const publicDir = join(root, "public");
+
+export default {
+  port: process.env.PORT || 3000,
+  dir: {
+    root,
+    audioDir,
+    publicDir,
+    songsDir: join(audioDir, "songs"),
+    fxDir: join(audioDir, "fx"),
+  },
+  pages: {
+    homeHTML: "home/index.html",
+    controllerHTML: "controller/index.html",
+  },
+  location: {
+    home: "/home",
+  },
+};
